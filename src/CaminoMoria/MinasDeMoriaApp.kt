7113751d8e85814c8d299edcce5ad4f49e7a4fde
Jorge Arenas <jorgearenass97@gmail.com>
2020-10-06 22:46:54 +0200

    Listo para entrar en las salas

--- src/CaminoMoria/MinasDeMoriaApp.kt
+++ src/CaminoMoria/MinasDeMoriaApp.kt
@@ -1,22 +1,30 @@
 package CaminoMoria
 
+import jdk.swing.interop.SwingInterOpUtils
 import personajes.*
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
     val SALAS = 36
+    val MAXPODER=30
+    val MAXFLECHAS=20
     val salas = arrayListOf<Sala>()
-    var gandalf = Mago("Gandalf",15)
-    var legolas = Elfo("Légolas",15)
+    var gandalf = Mago("Gandalf",Random.nextInt(MAXPODER)+1)
+    var legolas = Elfo("Légolas",Random.nextInt(MAXFLECHAS)+1)
     var frodo = Hobbit("Frodo")
     generaSalas(salas, SALAS)
 
     println(gandalf.toString())
     println(legolas.toString())
     println(frodo.toString())
+    caminoMoria(salas,gandalf,legolas,frodo)
 }
-
-fun generaSalas(salas: ArrayList<Sala>, SALAS: Int) {
+fun caminoMoria(salas:ArrayList<Sala>,gandalf:Mago,legolas:Elfo,frodo:Hobbit){
+    for(i in 0..(salas.size-1)){
+        println("Entrando en la sala ${i+1}...")
+    }
+}
+fun generaSalas(salas: ArrayList<Sala>,SALAS:Int) {
     var pel: Peligro = Peligro.MAGICO
     for (i in 0..(SALAS - 1)) {
         var rd = Random.nextInt(3) + 1
